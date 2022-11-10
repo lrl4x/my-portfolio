5a9c54ab26193b17f406f3be34e3d36d02cdff3c
Hamza Osailan <xihmozy_x_@hotmail.com>
2022-11-10 13:38:15 +0300

    n v

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,17 @@
-import { makeStyles, InputBase } from '@material-ui/core'
+import { makeStyles} from '@material-ui/core'
 import { Container, Box, Typography, Grid, TextField, Button, Stack } from '@mui/material'
 import React from 'react'
 import TerminalTwoToneIcon from '@mui/icons-material/TerminalTwoTone';
-import Projects_card from '../comp/Projects_card'
+import Card1 from '../comp/Card1'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import SendIcon from '@mui/icons-material/Send';
 import '../App.css'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { motion } from "framer-motion"
+import tr from '../tr.png'
+import plants from '../plants.png'
 
-{/*themeE*/ }
+//themeE
 const theme = createTheme({
   typography: {
     fontFamily:
@@ -21,10 +23,10 @@ const theme = createTheme({
     },
   },
 });
-{/*themeE*/ }
-const color = "red";
-const useStyles = makeStyles({
 
+
+//themecss
+const useStyles = makeStyles({
   arrowicon: {
     display: 'flex',
     justifyContent: 'center',
@@ -37,26 +39,9 @@ const useStyles = makeStyles({
     color: '#a9a9a9'
 
   },
-  TextField: {
-    borderWidth: "1px",
-    borderColor: "yellow !important"
-  }
 })
 
 
-{/*animation*/ }
-const textAnimate = {
-  offscreen: { x: -100 },
-  onscreen: {
-    x: 0,
-    rotate: [0, 90, -90, 90, 0],
-    Transition: {
-      type: "Spring",
-      bounce: 0.4,
-      duration: 3
-    }
-  }
-}
 
 export default function Main(props) {
   const classes = useStyles()
@@ -146,13 +131,13 @@ export default function Main(props) {
          }}
         >
           <Grid item xs={4}>
-            <Projects_card name='TreeCourse' image='' alt='TreeCourseImage' height="140" component="img" btn='share' dsc='In this project, we analyze designed and analysis application for giving solution of problem and can we solve this solution the problem is how students can find courses on one web/mobile application instead of multiple web/mobile application.' />
+            <Card1 name='TreeCourse' image={tr} alt='TreeCourseImage' height="140" component="img" btn='share' dsc='In this project, we analyze designed and analysis application for giving solution of problem and can we solve this solution the problem is how students can find courses on one web/mobile application instead of multiple web/mobile application.' />
           </Grid>
           <Grid item xs={4}>
-            <Projects_card name='PlantsWorld' image='tr.png' alt='PlantsWorld' height="140" component="img" btn='share' dsc='plantsworld is web page talking about the types of plants ' />
+            <Card1 name='PlantsWorld' image={plants} alt='PlantsWorld' height="140" component="img" btn='share' dsc='plantsworld is web page talking about the types of plants ' />
           </Grid>
           <Grid item xs={4}>
-            <Projects_card name='' image='tr.png' alt='PlantsWorld' height="140" component="img" btn='share' dsc='' />
+            <Card1 name='' image='tr.png' alt='PlantsWorld' height="140" component="img" btn='share' dsc='' />
           </Grid>
 
         </Grid>
@@ -180,40 +165,40 @@ export default function Main(props) {
 
         >
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://www.pinclipart.com/picdir/big/102-1024697_related-wallpapers-node-js-logo-png-clipart.png' width={70} />
+            <img src='https://www.pinclipart.com/picdir/big/102-1024697_related-wallpapers-node-js-logo-png-clipart.png'alt='' width={70} />
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://cdn.iconscout.com/icon/free/png-256/css-118-569410.png' width={70} />
+            <img src='https://cdn.iconscout.com/icon/free/png-256/css-118-569410.png' width={70} alt='' />
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://th.bing.com/th/id/R.2395ce87da3f91b4cdb179ec21ce7768?rik=9742j907NCeMAw&pid=ImgRaw&r=0' width={70} />
+            <img src='https://th.bing.com/th/id/R.2395ce87da3f91b4cdb179ec21ce7768?rik=9742j907NCeMAw&pid=ImgRaw&r=0' width={70} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol-700x394.png' width={70} />
+            <img src='https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol-700x394.png' width={70} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://andrejgajdos.com/wp-content/uploads/2019/11/npm-logo.png?x24361' width={70} />
+            <img src='https://andrejgajdos.com/wp-content/uploads/2019/11/npm-logo.png?x24361' width={70} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://desarrollador-android.com/wp-content/uploads/2015/03/android_studio_logo-1024x1024.png' width={70} />
+            <img src='https://desarrollador-android.com/wp-content/uploads/2015/03/android_studio_logo-1024x1024.png' width={70} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://objectpartners.com/wp-content/uploads/2015/02/mongodb-logo.png' width={70} />
+            <img src='https://objectpartners.com/wp-content/uploads/2015/02/mongodb-logo.png' width={70} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://logospng.org/download/html-5/logo-html-5-1024.png' width={70} />
+            <img src='https://logospng.org/download/html-5/logo-html-5-1024.png' width={70} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://download.logo.wine/logo/Bootstrap_(front-end_framework)/Bootstrap_(front-end_framework)-Logo.wine.png' width={150} />
+            <img src='https://download.logo.wine/logo/Bootstrap_(front-end_framework)/Bootstrap_(front-end_framework)-Logo.wine.png' width={150} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://pixlok.com/wp-content/uploads/2021/05/flutter-logo.jpg' width={70} />
+            <img src='https://pixlok.com/wp-content/uploads/2021/05/flutter-logo.jpg' width={70} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png' width={70} />
+            <img src='https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png' width={70} alt=''/>
           </Grid>
           <Grid item xs={6} md={4} lg={2}>
-            <img src='https://jafs.es/wp-content/uploads/2020/06/react.png' width={70} />
+            <img src='https://jafs.es/wp-content/uploads/2020/06/react.png' width={70} alt=''/>
           </Grid>
         </Grid>
 
@@ -261,11 +246,11 @@ export default function Main(props) {
          }}
         >
           <Button size='small' href='www.linkedin.com/in/hamza-osailan'>
-            <img width={20} src='https://logospng.org/download/linkedin/logo-linkedin-icon-512.png' />
+            <img width={20} src='https://logospng.org/download/linkedin/logo-linkedin-icon-512.png' alt=''/>
           </Button>
 
           <Button href='https://github.com/' size='small'>
-            <img width={20} src='https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol-700x394.png' />
+            <img width={20} src='https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol-700x394.png' alt=''/>
           </Button>
         </Box>
       </ThemeProvider>
